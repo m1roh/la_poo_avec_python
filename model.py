689fbacff19cc1869b70b623e3224592d53e4b0d
Romain <romain@geev.fr>
2019-04-27 12:23:21 +0200

    Add attributes from json dictionnary

--- model.py
+++ model.py
@@ -1,12 +1,16 @@
+import json
+
+
 class Agent:
-    def __init__(self, agreeableness):
-        self.agreeableness = agreeableness
-
-    def say_hello(self, first_name):
-        return "Bien le bonjour " + first_name + " !"
+    def __init__(self, **agent_attributes):
+        for attr_name, attr_value in agent_attributes.items():
+            setattr(self, attr_name, attr_value)
 
 
-agent = Agent(0)
+def main():
+    for agent_attributes in json.load(open("agents-100k.json")):
+        agent = Agent(**agent_attributes)
+        print(agent.agreeableness)
 
-print(agent.say_hello('Romain'))
-print(agent.agreeableness)
+
+main()
